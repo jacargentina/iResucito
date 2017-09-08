2c9eef6358a2a6b9f35fa64238f252659e1f0861
Javier Castro <javiercastro@nexion.com.ar>
2017-09-08 20:21:20 -0300

    Agregado nativebase; primeras pruebas

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  Container,
+  Header,
+  Title,
+  Subtitle,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text
+} from 'native-base';
 
 export default class HomeScreen extends React.Component {
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
+    });
+  }
+
   render() {
     return (
-      <View>
-        <Text>Prueba</Text>
-      </View>
+      <Container>
+        <Header style={{backgroundColor: '#3589de'}}>
+          <Left>
+            <Button transparent>
+              <Icon name="menu" style={{color: 'white'}}/>
+            </Button>
+          </Left>
+          <Body>
+            <Title style={{color: 'white'}}>Salmos</Title>
+          </Body>
+          <Right />
+        </Header>
+      </Container>
     );
   }
 }
