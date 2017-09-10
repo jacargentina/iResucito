115b1957cd4623c4b9b7399844d8647406180b87
Javier Castro <javiercastro@nexion.com.ar>
2017-09-10 10:03:16 -0300

    Avances

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -16,12 +16,6 @@ import {
 } from 'native-base';
 
 export default class HomeScreen extends React.Component {
-  async componentWillMount() {
-    await Expo.Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
-    });
-  }
 
   render() {
     return (
