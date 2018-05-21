7758763199af979117b6be483b6f42c1beafeee8
Javier Castro <javier.alejandro.castro@gmail.com>
2018-05-20 23:23:41 -0300

    Ajustes migracion native-base 2.4.5

--- native-base-theme/components/Item.js
+++ native-base-theme/components/Item.js
@@ -7,13 +7,20 @@ export default (variables = variable) => {
     ".floatingLabel": {
       "NativeBase.Input": {
         height: 50,
-        top: 8
+        top: 8,
+        paddingTop: 3,
+        paddingBottom: 7
       },
       "NativeBase.Label": {
         top: 8
       },
       "NativeBase.Icon": {
-        top: 6
+        top: 6,
+        paddingTop: 8
+      },
+      "NativeBase.IconNB": {
+        top: 6,
+        paddingTop: 8
       }
     },
     ".fixedLabel": {
@@ -49,8 +56,7 @@ export default (variables = variable) => {
         alignSelf: Platform.OS === "ios" ? "stretch" : "flex-start",
         flex: 1,
         width: Platform.OS === "ios" ? null : variables.deviceWidth - 25,
-        fontSize: variables.inputFontSize,
-        lineHeight: variables.inputLineHeight - 6,
+        fontSize: variables.inputFontSize
       },
       flexDirection: null,
       height: variables.inputHeightBase + 15
@@ -93,8 +99,7 @@ export default (variables = variable) => {
       color: variables.inputColor,
       flex: 1,
       top: Platform.OS === "ios" ? 1.5 : undefined,
-      fontSize: variables.inputFontSize,
-      lineHeight: variables.inputLineHeight
+      fontSize: variables.inputFontSize
     },
     ".underline": {
       "NativeBase.Input": {
