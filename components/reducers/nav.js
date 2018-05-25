90b9ffb0b1a9d838b95e91e746e51a70830ada8b
Javier Castro <javier.alejandro.castro@gmail.com>
2018-05-25 20:24:11 -0300

    WIP: Flow

--- components/reducers/nav.js
+++ components/reducers/nav.js
@@ -3,7 +3,7 @@ import AppNavigator from '../AppNavigator';
 
 const initialState = AppNavigator.router.getStateForAction('Menu');
 
-export default function nav(state = initialState, action) {
+export default function nav(state: any = initialState, action: any) {
   const nextState = AppNavigator.router.getStateForAction(action, state);
   return nextState || state;
 }
