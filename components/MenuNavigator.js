e99a62f1c024e8e0af4aebbc0b4b26719dd84753
Javier Castro <javier.alejandro.castro@gmail.com>
2018-07-11 10:12:58 -0300

    MenuNavigator simplificado

--- components/MenuNavigator.js
+++ components/MenuNavigator.js
@@ -1,10 +1,34 @@
 // @flow
+import { Platform } from 'react-native';
+import commonTheme from '../native-base-theme/variables/platform';
 import { createBottomTabNavigator } from 'react-navigation';
 import SalmoSearch from './screens/SalmoSearch';
 import SettingsScreen from './screens/SettingsScreen';
 import ListScreen from './screens/ListScreen';
 import CommunityScreen from './screens/CommunityScreen';
-import MenuNavigatorConfig from './MenuNavigatorConfig';
+
+var tabBarOptions = {};
+tabBarOptions.showLabel = false;
+tabBarOptions.activeTintColor = commonTheme.brandPrimary;
+tabBarOptions.style = {
+  backgroundColor: 'white'
+};
+
+if (Platform.OS == 'android') {
+  tabBarOptions.inactiveTintColor = 'gray';
+  tabBarOptions.style = {
+    backgroundColor: 'white'
+  };
+  tabBarOptions.pressColor = commonTheme.brandPrimary;
+  tabBarOptions.iconStyle = {
+    height: 30
+  };
+  tabBarOptions.indicatorStyle = {
+    backgroundColor: tabBarOptions.activeTintColor,
+    height: 3
+  };
+  tabBarOptions.showIcon = true;
+}
 
 const MenuNavigator = createBottomTabNavigator(
   {
@@ -13,7 +37,24 @@ const MenuNavigator = createBottomTabNavigator(
     Community: { screen: CommunityScreen },
     Settings: { screen: SettingsScreen }
   },
-  MenuNavigatorConfig
+  {
+    swipeEnabled: false,
+    tabBarOptions: tabBarOptions,
+    navigationOptions: (props: any) => {
+      var navigation = props.navigation;
+      switch (navigation.state.routeName) {
+        case 'Search':
+          return SalmoSearch.navigationOptions;
+        case 'Lists':
+          return ListScreen.navigationOptions;
+        case 'Community':
+          return CommunityScreen.navigationOptions;
+        case 'Settings':
+          return SettingsScreen.navigationOptions;
+      }
+      return null;
+    }
+  }
 );
 
 export default MenuNavigator;
