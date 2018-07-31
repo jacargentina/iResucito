ed24a6eca09d60548a3fab252347b4e2302dea7b
Javier Castro <javier.alejandro.castro@gmail.com>
2018-07-30 23:02:18 -0300

    Agregado utilitario applyPatch.js

---
+++ applyPatch.js
@@ -0,0 +1,21 @@
+var SongsIndex = require('./songs/index.json');
+var fs = require('fs');
+
+if (process.argv.length == 3) {
+  var path = process.argv[2];
+  if (path !== '') {
+    var json = fs.readFileSync(path, 'utf8').normalize();
+    var patch = JSON.parse(json);
+    Object.entries(patch).forEach(([key, value]) => {
+      var songToPatch = SongsIndex[key];
+      Object.assign(songToPatch.files, value);
+    });
+    console.log(SongsIndex);
+    fs.writeFileSync(
+      './songs/index.json',
+      JSON.stringify(SongsIndex, null, ' ')
+    );
+  }
+} else {
+  console.log('node applyPatch Path/To/SongsIndexPatch.json');
+}
