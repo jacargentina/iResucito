4945120f398fa2ca26b862778644e4eb946dbe0d
Javier Castro <javier.alejandro.castro@gmail.com>
2018-08-14 10:49:39 -0300

    Fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ var config = {
     path: path.resolve(__dirname, 'bin'),
     filename: '[name].js'
   },
+  target: 'node',
   module: {
     rules: [
       {
@@ -14,15 +15,8 @@ var config = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-flow'],
-            plugins: [
-              new webpack.BannerPlugin({
-                banner: '#!/usr/bin/env node',
-                raw: true
-              }),
-              '@babel/plugin-proposal-object-rest-spread',
-              '@babel/plugin-proposal-class-properties'
-            ]
+            babelrc: false,
+            presets: ['@babel/preset-env', '@babel/preset-flow']
           }
         }
       }
@@ -30,7 +24,13 @@ var config = {
   },
   resolve: {
     extensions: ['.js']
-  }
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: '#!/usr/bin/env node',
+      raw: true
+    })
+  ]
 };
 
 module.exports = config;
