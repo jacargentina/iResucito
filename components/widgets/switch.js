82963fb987305d6bda328ce5d2dd6bf77544bd35
Javier Castro <javier.alejandro.castro@gmail.com>
2018-09-12 11:28:08 -0300

    widgets/switch.js: refactorizacion por migracion RN 0.57

--- components/widgets/switch.js
+++ components/widgets/switch.js
@@ -5,12 +5,16 @@ import { Switch as SwitchNB } from 'native-base';
 import commonTheme from '../../native-base-theme/variables/platform';
 
 const Switch = (props: any) => {
-  var themeProps =
-    Platform.OS == 'android'
-      ? { thumbTintColor: commonTheme.brandPrimary, onTintColor: null }
-      : { onTintColor: null };
-  themeProps.onTintColor =
-    Platform.OS == 'ios' ? commonTheme.brandPrimary : commonTheme.brandLight;
+  var themeProps = {
+    trackColor: {
+      true: commonTheme.brandPrimary,
+      false: commonTheme.brandLight
+    },
+    thumbColor: null
+  };
+  if (Platform.OS === 'android') {
+    themeProps.thumbColor = commonTheme.brandPrimary;
+  }
   return <SwitchNB {...props} {...themeProps} />;
 };
 
