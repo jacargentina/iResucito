55b2e4e3eabc7bb68ee8cfc561410845cf7b74a9
Javier Castro <javier.alejandro.castro@gmail.com>
2018-11-14 09:51:21 -0300

    Ajuste para iPhone X/XS/XS Max: usar SafeAreaView en diálogos

--- components/screens/BaseModal.js
+++ components/screens/BaseModal.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
 import { Text, Icon } from 'native-base';
-import { View, Platform } from 'react-native';
+import { View, Platform, SafeAreaView } from 'react-native';
 import Modal from 'react-native-modal';
 import commonTheme from '../../native-base-theme/variables/platform';
 
@@ -33,44 +33,46 @@ const BaseModal = (props: any) => {
       onBackdropPress={() => props.closeModal()}
       onModalHide={() => props.modalHide()}
       onModalShow={() => props.modalShow()}>
-      <View
-        style={{
-          flex: 1,
-          paddingTop: Platform.OS == 'ios' ? 23 : 0,
-          backgroundColor: 'white'
-        }}>
-        <View
-          style={{
-            flex: 0,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            margin: 5
-          }}>
-          <Text
-            style={{
-              fontSize: commonTheme.fontSizeBase + 3,
-              fontWeight: 'bold',
-              paddingLeft: 10
-            }}>
-            {props.title}
-          </Text>
-          {closeButton}
-        </View>
+      <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
         <View
           style={{
             flex: 1,
+            paddingTop: Platform.OS == 'ios' ? 23 : 0,
+            backgroundColor: 'white'
           }}>
-          {props.children}
+          <View
+            style={{
+              flex: 0,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              margin: 5
+            }}>
+            <Text
+              style={{
+                fontSize: commonTheme.fontSizeBase + 3,
+                fontWeight: 'bold',
+                paddingLeft: 10
+              }}>
+              {props.title}
+            </Text>
+            {closeButton}
+          </View>
+          <View
+            style={{
+              flex: 1
+            }}>
+            {props.children}
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'flex-end'
+            }}>
+            {props.acceptButtons}
+          </View>
         </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'flex-end'
-          }}>
-          {props.acceptButtons}
-        </View>
-      </View>
+      </SafeAreaView>
     </Modal>
   );
 };
