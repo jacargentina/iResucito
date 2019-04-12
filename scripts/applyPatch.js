7fe54a600f980014fa44b82369bc1ca3c197f624
Javier Castro <javier.alejandro.castro@gmail.com>
2019-04-12 13:18:15 -0300

    Aplicado parche de-AT (F. Colautti)

--- scripts/applyPatch.js
+++ scripts/applyPatch.js
@@ -10,35 +10,41 @@ if (process.argv.length == 3) {
     var json = fs.readFileSync(patchPath, 'utf8').normalize();
     var patch = JSON.parse(json);
     Object.entries(patch).forEach(([key, value]) => {
-      var songToPatch = SongsIndex[key];
-      Object.entries(value).forEach(([locale, item]) => {
-        var { file, rename, lines } = item;
-        rename = rename.trim();
-        var oldName = path.resolve(`../songs/${locale}/${file}.txt`);
-        var newName = rename
-          ? path.resolve(`../songs/${locale}/${rename}.txt`)
-          : null;
-        var existeNuevo = newName && fs.existsSync(newName);
-        if (existeNuevo || !fs.existsSync(oldName)) {
-          console.log(`Key ${key}, renombrado previamente.`);
-        } else if (newName) {
-          execSync(`git mv --force "${oldName}" "${newName}"`);
-          Object.assign(songToPatch.files, { [locale]: rename });
-        } else {
-          Object.assign(songToPatch.files, { [locale]: file });
-        }
-        if (lines) {
-          const theSong = songToPatch.files[locale];
-          const songPath = path.resolve(`../songs/${locale}/${theSong}.txt`);
-          const text = fs.readFileSync(songPath, 'utf8');
-          if (text === lines) {
-            console.log(`Key ${key}, texto no aplicable.`);
-          } else {
-            fs.writeFileSync(songPath, lines);
-            console.log(`Key ${key}, aplicado texto.`);
+      try {
+        var songToPatch = SongsIndex[key];
+        Object.entries(value).forEach(([locale, item]) => {
+          var { file, rename, lines } = item;
+          if (rename) {
+            rename = rename.trim();
           }
-        }
-      });
+          var oldName = path.resolve(`../songs/${locale}/${file}.txt`);
+          var newName = rename
+            ? path.resolve(`../songs/${locale}/${rename}.txt`)
+            : null;
+          var existeNuevo = newName && fs.existsSync(newName);
+          if (existeNuevo || !fs.existsSync(oldName)) {
+            console.log(`Key ${key}, renombrado previamente.`);
+          } else if (newName) {
+            execSync(`git mv --force "${oldName}" "${newName}"`);
+            Object.assign(songToPatch.files, { [locale]: rename });
+          } else {
+            Object.assign(songToPatch.files, { [locale]: file });
+          }
+          if (lines) {
+            const theSong = songToPatch.files[locale];
+            const songPath = path.resolve(`../songs/${locale}/${theSong}.txt`);
+            const text = fs.readFileSync(songPath, 'utf8');
+            if (text === lines) {
+              console.log(`Key ${key}, texto no aplicable.`);
+            } else {
+              fs.writeFileSync(songPath, lines);
+              console.log(`Key ${key}, aplicado texto.`);
+            }
+          }
+        });
+      } catch (err) {
+        console.log(`Key ${key}`, err);
+      }
     });
     fs.writeFileSync(indexPath, JSON.stringify(SongsIndex, null, ' '));
   }
