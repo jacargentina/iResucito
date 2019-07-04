0beeae6d0a6adcc6d96600fbf1a77dd8131f171c
Javier Castro <javier.alejandro.castro@gmail.com>
2019-07-04 19:43:33 -0300

    Migracion RN 0.60

--- android/app/src/main/java/com/javiercastro/iresucito/MainApplication.java
+++ android/app/src/main/java/com/javiercastro/iresucito/MainApplication.java
@@ -5,19 +5,7 @@ import android.content.Context;
 import android.support.multidex.MultiDex;
 
 import com.facebook.react.ReactApplication;
-import cl.json.RNSharePackage;
-import com.reactcommunity.rnlocalize.RNLocalizePackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.masteratul.exceptionhandler.ReactNativeExceptionHandlerPackage;
-import com.RNFetchBlob.RNFetchBlobPackage;
-import com.christopherdro.RNPrint.RNPrintPackage;
-import org.wonday.pdf.RCTPdfView;
-import com.rt2zz.reactnativecontacts.ReactNativeContacts;
-import com.corbt.keepawake.KCKeepAwakePackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.oblador.vectoricons.VectorIconsPackage;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import com.rnfs.RNFSPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -38,19 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSharePackage(),
-            new RNLocalizePackage(),
-            new RNGestureHandlerPackage(),
-            new ReactNativeExceptionHandlerPackage(),
-            new RNFetchBlobPackage(),
-            new RNPrintPackage(),
-            new RCTPdfView(),
-            new ReactNativeContacts(),
-            new KCKeepAwakePackage(),
-            new RNDeviceInfo(),
-            new VectorIconsPackage(),
-            new SplashScreenReactPackage(),
-            new RNFSPackage()
+            new VectorIconsPackage()
       );
     }
 
