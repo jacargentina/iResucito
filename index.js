53794fdbaa2e50d057dde11f7bb82b3a715ff160
Javier Castro <javier.alejandro.castro@gmail.com>
2019-07-31 10:07:45 -0300

    Deshabilitado yellowbox

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './rn/App';
 import { name as appName } from './app.json';
+console.disableYellowBox = true;
 
 AppRegistry.registerComponent(appName, () => App);
