e69a20842d6e9578ab148873cd08af805c8151bb
Javier Castro <javier.alejandro.castro@gmail.com>
2019-08-18 19:40:14 -0300

    getOriginalPos.js: poder utilizar parametros via linea de comnados

--- scripts/getOriginalPos.js
+++ scripts/getOriginalPos.js
@@ -1,16 +1,34 @@
 const fs = require('fs');
 const SourceMap = require('source-map');
 
-const iosSourceMap = fs.readFileSync(
-  '../main.jsbundle.map',
-  'utf-8'
-);
+function run(file, column, line) {
+  const iosSourceMap = fs.readFileSync(file, 'utf-8');
 
-const parsed = JSON.parse(iosSourceMap);
-new SourceMap.SourceMapConsumer(parsed).then(mapConsumer => {
-  const result = mapConsumer.originalPositionFor({
-    line: 1024,
-    column: 588
+  const parsed = JSON.parse(iosSourceMap);
+  new SourceMap.SourceMapConsumer(parsed).then(mapConsumer => {
+    const result = mapConsumer.originalPositionFor({
+      line: Number(line),
+      column: Number(column)
+    });
+    console.log({ file, line, column, result });
   });
-  console.log({ result });
-});
+}
+
+var program = require('commander');
+
+program
+  .version('1.0')
+  .description('Get original position from soure map')
+  .option('-f, --file [path]', 'Path to map file')
+  .option('-c, --column [number]', 'Column number')
+  .option('-l, --line [number]', 'Line number');
+
+if (!process.argv.slice(2).length) {
+  program.help();
+} else {
+  program.parse(process.argv);
+  var file = program.file || '../main.jsbundle.map';
+  var column = program.column;
+  var line = program.line;
+  run(file, column, line);
+}
