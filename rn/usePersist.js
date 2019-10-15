d749aded1348d6061b1f0e93692694d78a25b3b6
Javier Castro <javier.alejandro.castro@gmail.com>
2019-10-15 12:04:03 -0300

    usePersist: agregada migracion de datos legacy de react-native-storage( ie { rawData: { ...} })

--- rn/usePersist.js
+++ rn/usePersist.js
@@ -20,6 +20,23 @@ const usePersist = (
     [typeCheck]
   );
 
+  const parseMigrateRawData = async str => {
+    var object = JSON.parse(str);
+    // si el str tiene formato de react-native-storage
+    // migrar a objeto sin propiedad 'rawData'
+    if (object.hasOwnProperty('rawData')) {
+      var migrated = {};
+      for (var prop in object.rawData) {
+        migrated[prop] = object.rawData[prop];
+      }
+      // almacenar con nuevo formato
+      const newdata = JSON.stringify(migrated);
+      await AsyncStorage.setItem(key, newdata);
+      object = migrated;
+    }
+    return object;
+  };
+
   const readFromStorage = useCallback(async () => {
     setValue(null);
     const theValue = await AsyncStorage.getItem(key);
@@ -27,7 +44,7 @@ const usePersist = (
       setValue(defValue);
       await AsyncStorage.setItem(key, JSON.stringify(defValue));
     } else {
-      const parsed = JSON.parse(theValue);
+      const parsed = await parseMigrateRawData(theValue);
       runTypeCheck(parsed);
       setValue(parsed);
     }
