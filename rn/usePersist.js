363812ae0f5db02d2221e6b9373b0bd45baac54d
Javier Castro <javier.alejandro.castro@gmail.com>
2020-12-08 16:21:55 -0300

    Actualizaciones npm

--- rn/usePersist.js
+++ rn/usePersist.js
@@ -1,6 +1,6 @@
 // @flow
 import { useState, useEffect, useCallback, useRef } from 'react';
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const persistGlobal = {};
 
