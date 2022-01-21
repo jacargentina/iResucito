51edc84e42c3cb2b78c497058917dca82d6199ba
Javier Castro <javier.alejandro.castro@gmail.com>
2022-01-21 14:39:29 -0300

    Refactor react-native-bootsplash v4

--- android/app/src/main/java/com/javiercastro/iresucito/MainActivity.java
+++ android/app/src/main/java/com/javiercastro/iresucito/MainActivity.java
@@ -1,15 +1,23 @@
 package com.javiercastro.iresucito;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
 import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
-    @Override
-    protected void onCreate(android.os.Bundle savedInstanceState) {
-        super.onCreate(null);
-        RNBootSplash.init(R.drawable.bootsplash, MainActivity.this);
-    }
+     @Override
+     protected ReactActivityDelegate createReactActivityDelegate() {
+       return new ReactActivityDelegate(this, getMainComponentName()) {
+    
+         @Override
+         protected void loadApp(String appKey) {
+           RNBootSplash.init(MainActivity.this);
+           super.loadApp(appKey);
+         }
+       };
+     }
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
