9b93a9b6aa81944973d5caced744dc3d143d78e1
Javier Castro <javier.alejandro.castro@gmail.com>
2022-09-10 13:14:46 -0300

    WIP migrar a RN 0.70

--- packages/native/android/app/src/main/java/com/javiercastro/iresucito/MainApplication.java
+++ packages/native/android/app/src/main/java/com/javiercastro/iresucito/MainApplication.java
@@ -43,7 +43,7 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    //initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
   /**
