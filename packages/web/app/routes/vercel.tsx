e3a9a72b38b5b3a0e8f64e9291d3a438092b6195
Javier Castro <javier.alejandro.castro@gmail.com>
2022-09-15 15:57:35 -0300

    WIP vercel.tsx

--- packages/web/app/routes/vercel.tsx
+++ packages/web/app/routes/vercel.tsx
@@ -1,40 +1,51 @@
 import path from 'path';
 import fs from 'fs';
-import { LoaderFunction } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { json, LoaderFunction, ActionFunction } from '@remix-run/node';
+import { useActionData, useLoaderData } from '@remix-run/react';
 import { dataPath } from '~/utils.server';
 import Layout from '~/components/Layout';
 
-export let loader: LoaderFunction = async () => {
+export let action: ActionFunction = async ({ request }) => {
+  const body = await request.formData();
+  const folder = (body.get('folder') as string) || '';
   var items: string[] = [];
-  const getAllFiles = (dirPath: string) => {
-    var files = fs.readdirSync(dirPath);
-    files.forEach((file: string) => {
-      if (fs.statSync(dirPath + '/' + file).isDirectory()) {
-        getAllFiles(dirPath + '/' + file);
-      } else {
-        const str = path.join(dirPath, '/', file);
-        items.push(` - ${str}`);
-      }
-    });
-  };
+  var error = null;
+  if (folder !== '') {
+    const getAllFiles = (dirPath: string) => {
+      var files = fs.readdirSync(dirPath);
+      files.forEach((file: string) => {
+        if (fs.statSync(dirPath + '/' + file).isDirectory()) {
+          getAllFiles(dirPath + '/' + file);
+        } else {
+          const str = path.join(dirPath, '/', file);
+          items.push(str);
+        }
+      });
+    };
+    try {
+      getAllFiles(folder);
+    } catch (err: any) {
+      error = err.message;
+    }
+    return json({ folder, items, error });
+  }
+  return null;
+};
 
-  getAllFiles(__dirname);
-
+export let loader: LoaderFunction = async () => {
   return {
     cwd: process.cwd(),
     dirname: __dirname,
     dataPath: dataPath,
-    items: items.sort(),
   };
 };
 
 const Vercel = () => {
   const data = useLoaderData();
-
+  const actionData = useActionData();
   return (
     <Layout>
-      <div style={{ height: '90%' }}>
+      <div style={{ height: '90%', padding: '10px' }}>
         <p>
           <strong>process.cwd(): {data.cwd}</strong>
         </p>
@@ -44,11 +55,26 @@ const Vercel = () => {
         <p>
           <strong>dataPath: {data.dataPath}</strong>
         </p>
-        <ul style={{ height: '100%', overflowY: 'scroll' }}>
-          {data.items.map((i: string) => {
-            return <li>{i}</li>;
-          })}
-        </ul>
+        <form method="post" style={{ padding: '10px' }}>
+          <label htmlFor="folder">Carpeta:&nbsp;</label>
+          <input id="folder" name="folder" type="text" />
+          <button type="submit">Consultar</button>
+        </form>
+        {actionData && (
+          <>
+            <p>
+              <strong>Folder: {actionData.folder}</strong>
+            </p>
+            {actionData.error && <pre>{actionData.error}</pre>}
+            <ul style={{ height: '100%', overflowY: 'scroll' }}>
+              {actionData
+                ? actionData.items.map((i: string) => {
+                    return <li>{i}</li>;
+                  })
+                : null}
+            </ul>
+          </>
+        )}
       </div>
     </Layout>
   );
