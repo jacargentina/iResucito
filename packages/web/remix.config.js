b8a984739564ead4b68fb20096957b4e73068ecf
Javier Castro <javier.alejandro.castro@gmail.com>
2022-09-21 18:36:32 -0300

    WIP ajustes (app remix con vercel)

--- packages/web/remix.config.js
+++ packages/web/remix.config.js
@@ -2,5 +2,7 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: 'vercel',
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   serverDependenciesToBundle: ['crypto-random-string', 'lowdb', 'steno'],
 };
