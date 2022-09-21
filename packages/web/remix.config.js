ab930355bfa61e6b993dadd0cb91d5f9d8a95bab
Javier Castro <javier.alejandro.castro@gmail.com>
2022-09-21 18:44:33 -0300

    WIP no funciona lo anterior

--- packages/web/remix.config.js
+++ packages/web/remix.config.js
@@ -2,7 +2,5 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: 'vercel',
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   serverDependenciesToBundle: ['crypto-random-string', 'lowdb', 'steno'],
 };
