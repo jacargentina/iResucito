d99bd3d2429ab20032e1d6e8135e71c4c6854728
Javier Castro <javiercastro@nexion.com.ar>
2023-05-22 08:20:26 -0300

    Reemplazar EN DASH por - normal

--- packages/core/tests/transport.test.ts
+++ packages/core/tests/transport.test.ts
@@ -2,7 +2,7 @@ import { SongsParser, PdfStyles } from '@iresucito/core';
 
 test('getChordsDiff', () => {
   var parser = new SongsParser(PdfStyles);
-  var diff_1 = parser.getChordsDiff('La–', 'Mi', 'es');
+  var diff_1 = parser.getChordsDiff('La-', 'Mi', 'es');
   expect(diff_1).toBe(-5);
   var diff_2 = parser.getChordsDiff('Do', 'Re', 'es');
   expect(diff_2).toBe(2);
@@ -12,10 +12,10 @@ test('getChordsDiff', () => {
 
 test('getChordsTransported', () => {
   var parser = new SongsParser(PdfStyles);
-  const result_1 = parser.getChordsTransported('La–', -5, 'es');
-  expect(result_1).toBe('Mi–');
-  const result_2 = parser.getChordsTransported('Re–', -5, 'es');
-  expect(result_2).toBe('La–');
+  const result_1 = parser.getChordsTransported('La-', -5, 'es');
+  expect(result_1).toBe('Mi-');
+  const result_2 = parser.getChordsTransported('Re-', -5, 'es');
+  expect(result_2).toBe('La-');
   const result_3 = parser.getChordsTransported('Mi', 2, 'es');
   expect(result_3).toBe('Fa#');
   const result_4 = parser.getChordsTransported('La', 3, 'es');
