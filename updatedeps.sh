2e3d4d2421e0d420e327653a55bd6449f01a3e86
Javier Castro <javiercastro@nexion.com.ar>
2024-04-11 16:21:20 -0300

    Centralizado uso de yarn (quitado npx)

--- updatedeps.sh
+++ updatedeps.sh
@@ -1,2 +1,2 @@
-find . -type f -name 'package.json' -not -path './.git/*' -not -path '*/node_modules/*' -execdir npx npm-check-updates --upgrade ";"
+find . -type f -name 'package.json' -not -path './.git/*' -not -path '*/node_modules/*' -execdir yarn npm-check-updates --upgrade ";"
 yarn
