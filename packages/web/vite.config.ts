524edae0bf1867c124e23ec638e5de021f58ca61
Javier Castro <javiercastro@nexion.com.ar>
2026-08-03 17:25:45 -0300

    web: aplicar flags y silenciar warnings

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -14,7 +14,19 @@ export default defineConfig({
       allow: ['..'],
     },
   },
-  plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths()],
+  plugins: [
+    remix({
+      presets: [vercelPreset()],
+      future: {
+        v3_fetcherPersist: true,
+        v3_lazyRouteDiscovery: true,
+        v3_relativeSplatPath: true,
+        v3_singleFetch: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
   build: {
     target: 'ES2022',
   },
